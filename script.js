0aa06633d994164666692b99eb744d219ebdf92e
Bollepalli Lakshmi Manogna <manumanogna126143@gmail.com>
2025-06-06 00:38:53 +0530

    indexpage

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-// console.log("✅ JavaScript is working!");
+// login form 
+
+document.getElementById("emailForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const inputEmail = document.getElementById("emailInput").value.trim();
+    const errorMsg = document.getElementById("errorMsg");
+
+    const allowedEmail = "admin@gmail.com";
+
+
+    if (inputEmail.toLowerCase() === allowedEmail.toLowerCase()) {
+      window.location.href = "profile.html";
+    } else {
+      errorMsg.classList.remove("hidden");
+    }
+  });
+
+
+//   trending now
+
+ const carousel = document.getElementById("trendingCarousel");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+
+  const scrollAmount = 300;
+
+  prevBtn.addEventListener("click", () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  nextBtn.addEventListener("click", () => {
+    carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+
+
+// questions
+document.querySelectorAll('.faq-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      const icon = button.querySelector('span');
+
+      if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+        icon.textContent = '−';
+      } else {
+        content.classList.add('hidden');
+        icon.textContent = '+';
+      }
+    });
+  });
